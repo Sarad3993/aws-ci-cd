c6f354400b8eb8c91b5b9487e8b43f25453a5792
Sharad Upadhyaya <risingstar3993@gmail.com>
2023-08-21 17:57:54 +0545

    Update app.py

--- simple-python-app/app.py
+++ simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello I am Sarad and I created ci/cd with aws successfully!'
+    return 'Hello I am Sharad Upadhyaya and I created ci/cd with aws successfully!'
 
 if __name__ == '__main__':
     app.run()
